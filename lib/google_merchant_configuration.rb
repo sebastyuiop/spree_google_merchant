1560580a71109e04a938dd60072f8a200c53ff3e
Asis Hallab <ah@mbp3.local>
2012-05-11 15:54:11 +0200

    Trying to fix Preferences.

--- lib/google_merchant_configuration.rb
+++ lib/google_merchant_configuration.rb
@@ -1,4 +1,4 @@
-class Spree::GoogleMerchantConfiguration < Spree::Configuration
+class GoogleMerchantConfiguration < Preferences::Configuration
   preference :google_merchant_title, :string, :default => 'My Spree Shop'
   preference :google_merchant_description, :string, :default => 'List of our products'
   preference :production_domain, :string, :default => 'http://www.myspreeshop.com/'
