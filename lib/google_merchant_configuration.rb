fcbf20ee4bcbce8d4cca0992581a7a6b2af2a7f9
Asis Hallab <ah@mbp3.local>
2012-05-11 15:49:05 +0200

    Fixing namespaces

--- lib/google_merchant_configuration.rb
+++ lib/google_merchant_configuration.rb
@@ -1,4 +1,4 @@
-class Spree::GoogleMerchantConfiguration < Configuration
+class Spree::GoogleMerchantConfiguration < Spree::Configuration
   preference :google_merchant_title, :string, :default => 'My Spree Shop'
   preference :google_merchant_description, :string, :default => 'List of our products'
   preference :production_domain, :string, :default => 'http://www.myspreeshop.com/'
